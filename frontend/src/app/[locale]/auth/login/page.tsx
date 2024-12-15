e07fd6354505e7a42d3fe43e7142d5fe3cc65384
kristzz <emils.krists@gmail.com>
2024-12-15 02:43:46 +0200

    login frontend init

---
+++ frontend/src/app/[locale]/auth/login/page.tsx
@@ -0,0 +1,13 @@
+import { useTranslations } from 'next-intl';
+
+export default function Login() {
+  const t = useTranslations('Login');
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <form>
+            
+        </form>
+    </main>
+  );
+}
--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -26,7 +26,7 @@ const Navbar = ({ locale }: { locale: string }) => {
 
 
   return (
-    <header className="flex w-full p-4 items-center justify-between dark:bg-darkBrown opacity-60 hover:opacity-90 duration-150">
+    <header className="flex w-full p-4 items-center justify-between dark:bg-darkBrown opacity-60 hover:opacity-90 duration-300">
       <div className="w-20"></div>
       <nav className="flex justify-center gap-8 flex-grow">
         <Link href={`/${locale}/jobs`} className="hover:text-lightbrown dark:hover:text-darkGreen duration-150">{t("jobs")}</Link>
@@ -37,8 +37,8 @@ const Navbar = ({ locale }: { locale: string }) => {
       <div className="w-20 flex justify-end items-center gap-4">
         {!isAuthenticated ? (
           <>
-            <Link href={`/${locale}/login`} className="text-sm hover:text-lightbrown dark:hover:text-darkGreen duration-150">{t("login")}</Link>
-            <Link href={`/${locale}/register`} className="text-sm hover:text-lightbrown dark:hover:text-darkGreen duration-150">{t("register")}</Link>
+            <Link href={`/${locale}/auth/login`} className="text-sm hover:text-lightbrown dark:hover:text-darkGreen duration-150">{t("login")}</Link>
+            <Link href={`/${locale}/auth/register`} className="text-sm hover:text-lightbrown dark:hover:text-darkGreen duration-150">{t("register")}</Link>
           </>
         ) : (
           <></>
@@ -48,7 +48,7 @@ const Navbar = ({ locale }: { locale: string }) => {
           value={locale}
           onChange={handleLanguageChange}
           aria-label={t("selectLanguage")}
-          className="border text-lightGray rounded-md px-2 py-1 focus:outline-none focus:ring-2 focus:ring-lightbrown dark:border-darkGreen dark:text-darkWhite dark:bg-darkBrown dark:focus:ring-darkGreen duration-150"
+          className="border text-lightGray rounded-md px-2 py-1 focus:ring-2 focus:ring-lightbrown dark:border-darkGreen dark:text-darkWhite dark:bg-darkBrown dark:focus:ring-darkGreen duration-150"
         >
           <option value="en">EN</option>
           <option value="lv">LV</option>
