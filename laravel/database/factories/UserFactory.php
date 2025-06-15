b19ec6a3b800c8ed3a0efa63c7e1f2cf9b581d42
kristsemils <krists.gabrielis@magebit.com>
2025-06-16 00:10:07 +0300

    vsfa

--- laravel/database/factories/UserFactory.php
+++ laravel/database/factories/UserFactory.php
@@ -29,6 +29,10 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'country' => fake()->country(),
+            'profession' => fake()->jobTitle(),
+            'role' => fake()->randomElement(['user', 'employer']),
+            'source' => fake()->optional()->randomElement(['website', 'mobile_app', 'referral']),
         ];
     }
 
